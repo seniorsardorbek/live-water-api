cda3719500591f0a79245b247e6477c1df91b488
seniorsardorbek <sardorbekmusilman@gmail.com>
2023-12-21 18:00:37 +0500

    secod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import config from './shared/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`${config.db.host}/${config.db.name}`),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
